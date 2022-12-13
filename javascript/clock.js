bb347ef3dcafbb8e0806c79cd3f3e5b756bf9902
vanessa-queiroga <116520236+vanessa-queiroga@users.noreply.github.com>
2022-12-13 20:21:38 +0000

    Added select option interaction with HTML so it display time when triggered

--- javascript/clock.js
+++ javascript/clock.js
@@ -2,29 +2,54 @@ function loadTime() {
   //London TimeZone
 
   let london = document.querySelector("#london");
-  let londonDate = document.querySelector("#london .date");
-  let londonTime = document.querySelector("#london .time");
-  let londonTimezone = moment().tz("Europe/London");
+  if (london) {
+    let londonDate = document.querySelector("#london .date");
+    let londonTime = document.querySelector("#london .time");
+    let londonTimezone = moment().tz("Europe/London");
 
-  londonDate.innerHTML = londonTimezone.format("dddd Mo [of] MMMM YYYY");
-  londonTime.innerHTML = londonTimezone.format("HH:mm:ss");
+    londonDate.innerHTML = londonTimezone.format("dddd Mo [of] MMMM YYYY");
+    londonTime.innerHTML = londonTimezone.format("HH:mm:ss");
+  }
 
   let tokyo = document.querySelector("#tokyo");
-  let tokyoDate = document.querySelector("#tokyo .date");
-  let tokyoTime = document.querySelector("#tokyo .time");
-  let tokyoTimezone = moment().tz("Asia/Tokyo");
+  if (tokyo) {
+    let tokyoDate = document.querySelector("#tokyo .date");
+    let tokyoTime = document.querySelector("#tokyo .time");
+    let tokyoTimezone = moment().tz("Asia/Tokyo");
 
-  tokyoDate.innerHTML = tokyoTimezone.format("dddd Mo [of] MMMM YYYY");
-  tokyoTime.innerHTML = tokyoTimezone.format("HH:mm:ss");
+    tokyoDate.innerHTML = tokyoTimezone.format("dddd Mo [of] MMMM YYYY");
+    tokyoTime.innerHTML = tokyoTimezone.format("HH:mm:ss");
+  }
 
   let australia = document.querySelector("#australia");
-  let australiaDate = document.querySelector("#australia .date");
-  let australiaTime = document.querySelector("#australia .time");
-  let australiaTimezone = moment().tz("Australia/Sydney");
+  if (australia) {
+    let australiaDate = document.querySelector("#australia .date");
+    let australiaTime = document.querySelector("#australia .time");
+    let australiaTimezone = moment().tz("Australia/Sydney");
 
-  australiaDate.innerHTML = australiaTimezone.format("dddd Mo [of] MMMM YYYY");
-  australiaTime.innerHTML = australiaTimezone.format("HH:mm:ss");
+    australiaDate.innerHTML = australiaTimezone.format(
+      "dddd Mo [of] MMMM YYYY"
+    );
+    australiaTime.innerHTML = australiaTimezone.format("HH:mm:ss");
+  }
 }
 
+function showTimezone(event) {
+  let timezoneMain = event.target.value;
+  let cityName = timezoneMain.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(timezoneMain);
+  let cityDropdown = document.querySelector("#cities");
+  cityDropdown.innerHTML = ` <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("HH:mm:ss")} </div>
+  </div>
+  `;
+}
+
+let cityElement = document.querySelector("#clock-dropdown");
+cityElement.addEventListener("change", showTimezone);
 loadTime();
 setInterval(loadTime, 1000);
